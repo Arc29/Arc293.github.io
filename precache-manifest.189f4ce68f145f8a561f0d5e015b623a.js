1c452c986d3380ccac2d1c54ea14b91f7472d12b
Aritra Chatterjee <aritrachatterjee07@gmail.com>
2019-06-28 21:15:04 +0530

    Updates

---
+++ precache-manifest.189f4ce68f145f8a561f0d5e015b623a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0a49d09da89e38f72c2dedeab4166a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c294054a650e11d0d8",
+    "url": "/static/css/2.11c68dcf.chunk.css"
+  },
+  {
+    "revision": "8ecbb91eb50196ef4267",
+    "url": "/static/css/main.b7d199c8.chunk.css"
+  },
+  {
+    "revision": "e1c294054a650e11d0d8",
+    "url": "/static/js/2.729ede05.chunk.js"
+  },
+  {
+    "revision": "8ecbb91eb50196ef4267",
+    "url": "/static/js/main.44b5735a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ca14f0440eb1723bf57414575fb993c",
+    "url": "/static/media/brain.7ca14f04.jpg"
+  }
+]);
